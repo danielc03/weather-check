bc66ca82fc3a2fea8a1f357cd9a54393a93f969d
c-daniel14 <cojocarugeani@yahoo.com>
2018-08-07 22:15:58 +0100

    weather api call

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 // Initialize weather object
 const weather = new Weather('London');
 
-weather.getWeather()
+
+const getWeather = () => { 
+    weather.getWeather()
     .then(results => console.log(results))
-    .catch(err => console.log(err))
+    .catch(err => console.log(err));
+}
+
+
+// Get weather on load
+document.addEventListener('DOMContentLoaded', getWeather);
