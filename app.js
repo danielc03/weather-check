850b9af1b87d09212e5e02f8046ab7e0cbb3dcf9
c-daniel14 <cojocarugeani@yahoo.com>
2018-08-09 20:49:21 +0100

    location change feature

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 // Initialize weather object
 const weather = new Weather('Cambridge');
+// Initialize UI
 const ui = new UI();
+
 const description = document.getElementById('w-desc');
 const body = document.getElementsByTagName('BODY')[0];
 
+// Get weather through API
 const getWeather = () => { 
     weather.getWeather()
     .then(results => {
@@ -13,17 +16,16 @@ const getWeather = () => {
     .catch(err => console.log(err));
 }
 
-
 // Get weather on load
 document.addEventListener('DOMContentLoaded', getWeather);
-
 
 
 
 // BG Depending on weather conditions
 const bgSet = () => {
     const desc = description.textContent.toString();
-    // const check = /cloud/i;
+    // Changing body class depending on weather
+    const check = () => {
     if(/clouds/i.test(desc)){
         body.classList.add("clouds"); 
     } else if(/rain/i.test(desc)){
@@ -39,8 +41,23 @@ const bgSet = () => {
     } else {
         body.classList.add("fog")
     }
+}
+check();    
 };
 setTimeout(bgSet,2000); 
 
 
+
+// Change location event
+document.getElementById('w-change-button').addEventListener('click', (e) => {
+    const city = document.getElementById('city').value;
+
+    weather.changeLocation(city);
+
+    // Get and display weather on location
+    getWeather();
+    setTimeout(bgSet, 3000);
+    // Close modal
+    $('#locModal').modal('hide');
+})
 
